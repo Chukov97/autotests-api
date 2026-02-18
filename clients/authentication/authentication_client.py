83f2c594f68fa696fea1d453065d31a7917dca8b
Ilya Chukov <ichukov@licard.com>
2026-02-18 12:57:10 +0300

    Улучшение работы с API клиентами с помощью Enum для URI. Практика работы с использованием APIRoutes в API-клиентах

--- clients/authentication/authentication_client.py
+++ clients/authentication/authentication_client.py
@@ -4,6 +4,7 @@ from httpx import Response
 from clients.api_client import APIClient
 from clients.public_http_builder import get_public_http_client
 from clients.authentication.authentication_schema import LoginRequestSchema, RefreshRequestSchema, LoginResponseSchema
+from tools.routes import APIRoutes
 
 
 class AuthenticationClient(APIClient):
@@ -20,7 +21,7 @@ class AuthenticationClient(APIClient):
         :return: Ответ от сервера в виде объекта httpx.Response
         """
         return self.post(
-            "/api/v1/authentication/login",
+            f"{APIRoutes.AUTHENTICATION}/login",
             json=request.model_dump(by_alias=True)
         )
 
@@ -33,8 +34,9 @@ class AuthenticationClient(APIClient):
         :return: Ответ от сервера в виде объекта httpx.Response
         """
         return self.post(
-            "/api/v1/authentication/refresh",
-            json=request.model_dump(by_alias=True))
+            f"{APIRoutes.AUTHENTICATION}/refresh",
+            json=request.model_dump(by_alias=True)
+        )
 
     def login(self, request: LoginRequestSchema) -> LoginResponseSchema:
         response = self.login_api(request)
